43f5d6188756b57fc28a395ab04d30127000b57d
nico <nicolauria@outlook.com>
2018-10-21 12:06:08 -0700

    finish User model, user login/signin

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const passport = require("passport");
+require("./config/passport")(passport);
+
+const bodyParser = require('body-parser');
 
 const mongoose = require("mongoose");
 const db = require('./config/keys').mongoURI;
@@ -12,3 +18,11 @@ app.get("/", (req, res) => res.send("hello world!"));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
---
+++ routes/api/users.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
+const jsonwebtoken = require("jsonwebtoken");
+const keys = require("../../config/keys")
+const passport = require('passport');
+
+router.post("/register", (req, res) => {
+  User.findOne({name: req.body.name}).then(user => {
+    if (user) {
+      errors.name = "User already exists";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      })
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err
+          newUser.password = hash
+          newUser.save()
+            .then(user => {
+              const payload = {id: user.id, name: user.name}
+              jsonwebtoken.sign(payload, keys.secretOrKey, {expiresIn: 3600}, (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                })
+              })
+            })
+        })
+      })
+    }
+  })
+})
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        errors.email = "This user doesn't exist";
+        return res.status(400).json(errors)
+      }
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          const payload = {id: user.id, name: user.name}
+          jsonwebtoken.sign(payload, keys.secretOrKey, {expiresIn: 3600}, (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            })
+          })
+        } else {
+          errors.password = "Incorrect password"
+          return res.status(400).json(errors)
+        }
+      })
+    })
+})
+
+router.get('/current', passport.authenticate('jsonwebtoken', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  })
+})
+
+router.get("/test", (req, res) => res.send("working users"));
+
+module.exports = router;
