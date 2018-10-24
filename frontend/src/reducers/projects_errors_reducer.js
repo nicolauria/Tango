36a95df265e789bfbdea98bfa5ee50c0be73f288
Brett <jbrettk629@gmail.com>
2018-10-24 11:09:29 -0700

    complete project and project errors reducer

---
+++ frontend/src/reducers/projects_errors_reducer.js
@@ -0,0 +1,17 @@
+import {RECEIVE_PROJECT_ERRORS, RECEIVE_ALL_PROJECTS, RECEIVE_PROJECT} from '../actions/projects_actions',
+
+const ProjectsErrorsReducer = (state = [], action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_ALL_PROJECTS:
+            return [];
+        case RECEIVE_PROJECT:
+            return [];
+        case RECEIVE_PROJECT_ERRORS:
+            return action.errors;
+        default:
+            return state;
+    }
+}
+
+export default ProjectsErrorsReducer;
---
+++ frontend/src/reducers/projects_reducer.js
@@ -0,0 +1,17 @@
+import {RECEIVE_ALL_PROJECTS, RECEIVE_PROJECT} from '../actions/projects_actions';
+import merge from 'lodash/merge';
+
+const ProjectsReducer = (state = [], action) => {
+    Object.freeze(state);
+    switch(action.type){
+        case RECEIVE_ALL_PROJECTS:
+            return action.projects,
+        case RECEIVE_PROJECT:
+            return state.concat(action.project);
+        default:
+            return state;
+
+    }
+}
+
+export default ProjectsReducer;
