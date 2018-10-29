e2611661f6ce3570ca99fd04e4e307135e1f9a52
nico <nicolauria@outlook.com>
2018-10-28 18:15:48 -0700

    add orange border to project show

--- frontend/src/reducers/projects_reducer.js
+++ frontend/src/reducers/projects_reducer.js
@@ -6,11 +6,11 @@ const ProjectsReducer = (state = {}, action) => {
     Object.freeze(state);
     switch(action.type){
         case RECEIVE_ALL_PROJECTS:
-        let newState = Object.assign({}, action.projects)
-          // let newState = {}
-          //   action.projects.forEach(project => {
-          //     newState[project._id] = project
-          //   });
+        // let newState = Object.assign({}, action.projects)
+          let newState = {}
+            action.projects.forEach(project => {
+              newState[project._id] = project
+            });
           return newState;
         case RECEIVE_PROJECT:
             return merge({}, state, {[action.project._id]: action.project});
